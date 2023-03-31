79884610cba1a9a88640521b1f969b8bbb352864
Mohammad-Javad-Ameri <110919726+Mohammad-Javad-Ameri@users.noreply.github.com>
2023-04-01 03:11:40 +0430

    start server side of project

---
+++ server/models/user.js
@@ -0,0 +1,58 @@
+const { model, Schema }  = require("mongoose")
+const jwt = require("jwt")
+const joi = require("joi")
+const passwordComplexity=require("joi-password-complexity")
+
+const userSchema=  new Schema({
+    name:{
+        type:String,
+        required: true
+    },
+     email:{
+        type:String,
+        required: true
+    },
+     password:{
+        type:String,
+        required: true
+    },
+     linkedin:{
+        type:String,
+        required: true
+    },
+     github:{
+        type:String,
+        required: true
+    },
+     role:{
+        type:String,
+        required: true   
+    },
+     skills:{
+        type:String,
+        required: true
+    },
+     language:{
+        type:String,
+        required: true
+    },  
+},
+ {timestamps:true}
+)
+
+userSchema.methods.generateAuthToken=function(){
+    const token=jwt.sign({_id:this._id},process.env.JWT_KEY)
+    return token;
+}
+const User = model("user",userSchema)
+
+const validate = (data)=>{
+    const schema=joi.opject({
+        name:joi.string().required().label("Name"),
+        email:joi.string().email().required().label("Email"),
+        password:passwordComplexity().required.label("password")
+
+    })
+    return schema.validate(data)
+}
+module.exports = {User,validate}
